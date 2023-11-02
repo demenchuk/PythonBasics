f23b6c386ce6137293b376e0dfdc0f84b8609c8d
Alex Demen <Alexandrdemenchukk@gmail.com>
2023-11-02 22:32:19 +0100

    DZ

--- main.py
+++ main.py
@@ -1,2 +1,18 @@
 
-print("Hallo word")22222
+
+n1 = int(input("Введіть перше число: "))
+n2 = int(input("Введіть друге число: "))
+n3 = int(input("Введіть третє число: "))
+
+
+max_num = max(n1, n2, n3)
+
+min_num = min(n1, n2, n3)
+
+
+middle_num = n1 + n2 + n3 - max_num - min_num
+
+
+print(max_num)
+print(middle_num)
+print(min_num)
