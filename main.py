97f2681978a17bfd538d5fa798fcfc1581efedd0
Alex Demen <Alexandrdemenchukk@gmail.com>
2023-11-20 18:15:34 +0100

    Fertig1

--- main.py
+++ main.py
@@ -1,18 +1,2 @@
-
-#Перше домащне завлання
-n1 = int(input("Введіть перше число: "))
-n2 = int(input("Введіть друге число: "))
-n3 = int(input("Введіть третє число: "))
-
-
-max_num = max(n1, n2, n3)
-
-min_num = min(n1, n2, n3)
-
-
-middle_num = n1 + n2 + n3 - max_num - min_num
-
-
-print(max_num)
-print(middle_num)
-print(min_num)
+import nltk
+nltk.download('punkt')
